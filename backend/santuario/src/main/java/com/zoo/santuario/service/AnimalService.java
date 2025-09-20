dd8340eac892bbebceaca99e667e94936735af0f
ariel439 <dolzanariel@gmail.com>
2025-09-20 19:00:40 -0300

    fix

--- backend/santuario/src/main/java/com/zoo/santuario/service/AnimalService.java
+++ backend/santuario/src/main/java/com/zoo/santuario/service/AnimalService.java
@@ -8,6 +8,8 @@ import com.zoo.santuario.repository.CuidadorRepository;
 import com.zoo.santuario.model.Cuidador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +17,8 @@ import java.util.stream.Collectors;
 
 @Service
 public class AnimalService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AnimalService.class);
 
     @Autowired
     private AnimalRepository animalRepository;
@@ -41,12 +45,16 @@ public class AnimalService {
 
         // Send email notification if keeperId is present
         if (savedAnimal.getKeeperId() != null) {
+            logger.info("Attempting to send email for new animal: {}", savedAnimal.getName());
             cuidadorRepository.findById(savedAnimal.getKeeperId()).ifPresent(cuidador -> {
+                logger.info("Cuidador found for keeperId: {}. Contact email: {}", savedAnimal.getKeeperId(), cuidador.getContact());
                 String subject = "New Animal Assigned: " + savedAnimal.getName();
                 String body = String.format("Dear %s,<br><br>A new animal, <b>%s</b> (Species: %s), has been assigned to you.<br><br>Regards,<br>Zoo Management",
                         cuidador.getName(), savedAnimal.getName(), savedAnimal.getSpecies());
                 emailService.sendAnimalNotificationEmail(cuidador.getContact(), subject, body);
             });
+        } else {
+            logger.info("No keeperId present for new animal: {}. Skipping email notification.", savedAnimal.getName());
         }
 
         return convertToDto(savedAnimal);
@@ -73,16 +81,23 @@ public class AnimalService {
 
                     // Send email notification if keeperId changed or is newly assigned
                     if (updatedAnimal.getKeeperId() != null && !updatedAnimal.getKeeperId().equals(oldKeeperId)) {
+                        logger.info("Attempting to send email for updated animal (new keeper): {}", updatedAnimal.getName());
                         cuidadorRepository.findById(updatedAnimal.getKeeperId()).ifPresent(cuidador -> {
+                            logger.info("Cuidador found for new keeperId: {}. Contact email: {}", updatedAnimal.getKeeperId(), cuidador.getContact());
                             String subject = "Animal Assignment Updated: " + updatedAnimal.getName();
                             String body = String.format("Dear %s,<br><br>The animal <b>%s</b> (Species: %s) has been assigned to you.<br><br>Regards,<br>Zoo Management",
                                     cuidador.getName(), updatedAnimal.getName(), updatedAnimal.getSpecies());
                             emailService.sendAnimalNotificationEmail(cuidador.getContact(), subject, body);
                         });
+                    } else if (updatedAnimal.getKeeperId() == null && oldKeeperId != null) {
+                        logger.info("Animal {} unassigned from keeper {}. Attempting to notify old keeper.", updatedAnimal.getName(), oldKeeperId);
+                    } else {
+                        logger.info("No keeperId change or new assignment for updated animal: {}. Skipping email notification.", updatedAnimal.getName());
                     }
                     // Optionally, notify the old keeper if assignment changed
                     if (oldKeeperId != null && (updatedAnimal.getKeeperId() == null || !updatedAnimal.getKeeperId().equals(oldKeeperId))) {
                          cuidadorRepository.findById(oldKeeperId).ifPresent(cuidador -> {
+                            logger.info("Cuidador found for old keeperId: {}. Contact email: {}", oldKeeperId, cuidador.getContact());
                             String subject = "Animal Unassigned: " + updatedAnimal.getName();
                             String body = String.format("Dear %s,<br><br>The animal <b>%s</b> (Species: %s) has been unassigned from you.<br><br>Regards,<br>Zoo Management",
                                     cuidador.getName(), updatedAnimal.getName(), updatedAnimal.getSpecies());
@@ -103,12 +118,16 @@ public class AnimalService {
 
             // Send email notification to keeper if assigned
             if (animalToDelete.getKeeperId() != null) {
+                logger.info("Attempting to send email for deleted animal: {}", animalToDelete.getName());
                 cuidadorRepository.findById(animalToDelete.getKeeperId()).ifPresent(cuidador -> {
+                    logger.info("Cuidador found for keeperId: {}. Contact email: {}", animalToDelete.getKeeperId(), cuidador.getContact());
                     String subject = "Animal Removed: " + animalToDelete.getName();
                     String body = String.format("Dear %s,<br><br>The animal <b>%s</b> (Species: %s), which was assigned to you, has been removed from the system.<br><br>Regards,<br>Zoo Management",
                             cuidador.getName(), animalToDelete.getName(), animalToDelete.getSpecies());
                     emailService.sendAnimalNotificationEmail(cuidador.getContact(), subject, body);
                 });
+            } else {
+                logger.info("No keeperId present for deleted animal: {}. Skipping email notification.", animalToDelete.getName());
             }
             return true;
         }
