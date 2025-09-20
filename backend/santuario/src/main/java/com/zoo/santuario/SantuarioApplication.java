392c082ee98622422b8813f6cb4d50ac62808c6e
ariel439 <dolzanariel@gmail.com>
2025-09-19 23:48:57 -0300

    debug env variables

--- backend/santuario/src/main/java/com/zoo/santuario/SantuarioApplication.java
+++ backend/santuario/src/main/java/com/zoo/santuario/SantuarioApplication.java
@@ -2,12 +2,19 @@ package com.zoo.santuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext; // Import this
 
 @SpringBootApplication
 public class SantuarioApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantuarioApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SantuarioApplication.class, args);
+		// Temporary logging to debug environment variables
+		System.out.println("DEBUG: SPRING_DATASOURCE_URL = " + context.getEnvironment().getProperty("SPRING_DATASOURCE_URL"));
+		System.out.println("DEBUG: MYSQL_HOST = " + context.getEnvironment().getProperty("MYSQL_HOST"));
+		System.out.println("DEBUG: MYSQL_PORT = " + context.getEnvironment().getProperty("MYSQL_PORT"));
+		System.out.println("DEBUG: MYSQL_DATABASE = " + context.getEnvironment().getProperty("MYSQL_DATABASE"));
+		// End temporary logging
 	}
 
 }
